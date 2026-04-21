dcdadd6d07f7f635425205ea10b0e8105dc321f2
grzegorz <niziolek15@gmail.com>
2026-04-21 04:03:07 -0400

    adding web app

---
+++ app.py
@@ -0,0 +1,27 @@
+import pickle 
+from flask import Flask, request, jsonify,app,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+regmodel = pickle.load(open('model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    data = request.json["data"]
+    predict_request = np.array(list(data.values())).reshape(1, -1)
+    predict_request = scaler.transform(predict_request)
+    prediction = regmodel.predict(predict_request) 
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
